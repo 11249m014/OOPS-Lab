f261d295396fc3e717e7bdd2b46c641d62f4dfcd
M Karthik <11249m014@kanchiuniv.ac.in>
2025-08-31 14:22:21 +0530

    senariobased 1.cpp

--- senariobased 1.cpp
+++ senariobased 1.cpp
@@ -1,32 +1,32 @@
 //A company manufactures Laptops. Each laptop has a brand name,
-//processor type, and RAM size.Write a class to represent a laptop, create
+//processor type, and RAM size. Write a class to represent a laptop, create
 //two objects with different details, and display them.
 
 #include<iostream>
 using namespace std;
 
-class Laptop{
+class Laptop {
 public:
     string brand;
     string processor;
     int ram;
 
-    void display(){
-      cout<<"Brand:"<<brand
-      cout<<"Processor:"<<processor
-      cout<<"RAM:"<<ram<<"GB"<<endl;
+    void display() {
+        cout << "Brand: " << brand << endl;
+        cout << "Processor: " << processor << endl;
+        cout << "RAM: " << ram << "GB" << endl;
     }
 };
 
-int main(){
-Laptop 11,12;
-11.brand="Dell";
-11.processor="Intel i5";
-11.ram=8;
-12.brand="HP";
-12.processor="AMD Ryzen 5";
-12.ram=16;
-11.display();
-12.display();
-return 0;
+int main() {
+    Laptop l1, l2;
+    l1.brand = "Dell";
+    l1.processor = "Intel i5";
+    l1.ram = 8;
+    l2.brand = "HP";
+    l2.processor = "AMD Ryzen 5";
+    l2.ram = 16;
+    l1.display();
+    l2.display();
+    return 0;
 }
